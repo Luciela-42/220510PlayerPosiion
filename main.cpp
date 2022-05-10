84545a157d4cdac48c6126739c1a03bf7e4ac6d3
Hvvi_LCLA <inhui99@gmail.com>
2022-05-10 18:38:54 +0900

    rand

--- main.cpp
+++ main.cpp
@@ -16,26 +16,59 @@
 
 using namespace std;
 
+
 struct FVector2D
 {
 	int X;
 	int Y;
 };
 
+char Map[10][10] = {
+	{'#','#','#','#','#','#','#','#','#','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ','#',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ','#',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#','#','#','#','#','#','#','#','#','#'}
+};
+
+
 void SetLocation(FVector2D NewLocation);
-void MapBoundary();
+
+void DrawBG(FVector2D StartPosition)
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			FVector2D Temp;
+			Temp.X = X + StartPosition.X;
+			Temp.Y = Y + StartPosition.Y;
+			SetLocation(Temp);
+			cout << Map[Y][X];
+		}
+	}
+
+}
 
 int main()
 {
 	bool bRunning = true;
 	FVector2D PlayerPosition;
-	PlayerPosition.X = 5;
-	PlayerPosition.Y = 6;
+	PlayerPosition.X = 0;
+	PlayerPosition.Y = 0;
 
+	srand(static_cast<unsigned int>(time(nullptr)));
+	FVector2D StartPosition;
+	StartPosition.X = rand() % 10 + 1;
+	StartPosition.Y = rand() % 10 + 1;
 
 	while (bRunning)
 	{
-
 		int KeyCode = _getch();
 
 		switch (KeyCode)
@@ -57,24 +90,30 @@ int main()
 			PlayerPosition.X++;
 			break;
 		case 27:
-			bRunning = 0;
+			bRunning = false;
+			break;
+
 		}
 
 		PlayerPosition.X = PlayerPosition.X < 1 ? 1 : PlayerPosition.X;
 		PlayerPosition.Y = PlayerPosition.Y < 1 ? 1 : PlayerPosition.Y;
-		PlayerPosition.X = PlayerPosition.X > 8 ? 8 : PlayerPosition.X;
-		PlayerPosition.Y = PlayerPosition.Y > 8 ? 8 : PlayerPosition.Y;
+		PlayerPosition.X = PlayerPosition.X >= 9 ? 8 : PlayerPosition.X;
+		PlayerPosition.Y = PlayerPosition.Y >= 9 ? 8 : PlayerPosition.Y;
 
 		system("cls");
 
-		MapBoundary();
-
-		SetLocation(PlayerPosition);
+		DrawBG(StartPosition);
+		FVector2D Temp;
+		Temp.X = PlayerPosition.X + StartPosition.X;
+		Temp.Y = PlayerPosition.Y + StartPosition.Y;
+		SetLocation(Temp);
 		cout << "P";
 	}
 
+
 	return 0;
 }
+
 
 void SetLocation(FVector2D NewLocation)
 {
@@ -83,49 +122,3 @@ void SetLocation(FVector2D NewLocation)
 	Cur.Y = NewLocation.Y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
-
-void MapBoundary()
-{
-	int Map[10][10] =
-	{
-		1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-	};
-
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = 0; j < 10; ++j)
-		{
-			switch (Map[i][j])
-			{
-			case 0:
-				cout << " ";
-				break;
-			case 1:
-				cout << "@";
-				break;
-			}
-			
-		}
-		cout << endl;
-	}
-	//cout << "##########" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "#	 #" << endl;
-	//cout << "##########" << endl;
-}
-
